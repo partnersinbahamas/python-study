9f3b9c312c06a42126b8d1c407757a74d12d0dd2
Denys Bokov <denisbokov1703@gmail.com>
2024-09-23 20:07:25 +0200

    feat: print with special \ symbol to print douple quote in double quotes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # TIP: to create .venv folder please type: python3 -m venv .venv
 print('Hello world!')
 print('Mixed print', 5, sep=': ') # sep= using to indicates what will be between
-print('Print with end', end=" End of line")
+print('Print with end', end=" End of line\n")
+print("Print quore \" ")
