293166a360c12e6cac1dadd66e4a5471c6ae3323
Denys Bokov <denisbokov1703@gmail.com>
2024-09-23 20:17:07 +0200

    feat: number round math operation with //

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ print('Hello world!')
 print('Mixed print', 5, sep=': ') # sep= using to indicates what will be between
 print('Print with end', end=" End of line\n")
 print("Print quore \" ")
+print('Result of 5 / 3 =', 5//3) # // = round the result number to 0 characters after comma
