b8304bc4b470e221c5ae6f939dbf5906ffed441d
Denys Bokov <denys.bokov@jonasundderwolf.de>
2024-09-23 20:03:49 +0200

    feat: mixed print with end key word

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 # TIP: to create .venv folder please type: python3 -m venv .venv
 print('Hello world!')
 print('Mixed print', 5, sep=': ') # sep= using to indicates what will be between
+print('Print with end', end=" End of line")
