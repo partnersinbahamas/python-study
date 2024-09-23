90947582f607eaa53a65be5a64a0722595d32fee
Denys Bokov <denisbokov1703@gmail.com>
2024-09-23 20:20:42 +0200

    feat: print minimal and maximal number using min(), max()

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ print('Mixed print', 5, sep=': ') # sep= using to indicates what will be between
 print('Print with end', end=" End of line\n")
 print("Print quore \" ")
 print('Result of 5 / 3 =', 5//3) # // = round the result number to 0 characters after comma
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print('Minimal number:', min(nums))
+print('Maximal number:', max(nums))
