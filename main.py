5c1e3cca69d2b5d0bcaf70ad223267ce9e3ee97b
Denys Bokov <denisbokov1703@gmail.com>
2024-09-23 20:27:06 +0200

    feat: round number with round()

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ print('Hello world!')
 print('Mixed print', 5, sep=': ') # sep= using to indicates what will be between
 print('Print with end', end=" End of line\n")
 print("Print quore \" ")
-print('Result of 5 / 3 =', 5//3) # // = round the result number to 0 characters after comma
+print('Result of 5 / 3 with // =', 5//3) # // = round the result number to less to 0 characters after comma
+print('Result of 5 / 3 wiht round =', round(5 / 3)) # round the result number to more to 0 characters after comma
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print('Minimal number:', min(nums))
