2654ee9ead9a342f9e2ab6e48c0991dd5629eabf
Denys Bokov <denisbokov1703@gmail.com>
2024-09-24 18:34:54 +0200

    fixup! feat: other vars with python data types

--- variables.py
+++ variables.py
@@ -2,9 +2,9 @@ number = 10
 del number # delete number
 # print(number) => error cause we have deleted this var
 
-number = 1 # integer
+number = 1 # int
 digit = 17.032004 # float
-string = 'Hello string' # string
+string = 'Hello string' # str
 boolean = True # bool 
 
 print('number: ', number)
