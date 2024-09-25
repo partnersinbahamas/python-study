2ac53e0031ea0258ae7e859358062213a6e4f81a
Denys Bokov <denisbokov1703@gmail.com>
2024-09-25 18:42:37 +0200

    feat: fist conditional operatior

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+user_data = int(input('Please enter any number:'))
+
+
+
+if user_data > 0:
+    print('Your number is more then 0 | Input: ', user_data)
+    if user_data > 10:
+        print('Your number is more then 10 | Input: ', user_data)
+else:
+   print('Your number is less then 0 | Input: ', user_data)
