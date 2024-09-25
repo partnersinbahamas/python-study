3982af4a18589af0c2cb952aa212cf7a6c553840
Denys Bokov <denisbokov1703@gmail.com>
2024-09-25 18:59:29 +0200

    feat: condition with or, and

--- conditionals.py
+++ conditionals.py
@@ -4,6 +4,8 @@ if user_data > 0:
     print('Your number is more then 0 | Input: ', user_data)
     if user_data > 10:
         print('Your number is more then 10 | Input: ', user_data)
+    elif user_data == 10:
+      print('Your number is 10')
 else:
    print('Your number is less then 0 | Input: ', user_data)
 
@@ -13,3 +15,9 @@ if not user_bool_data == '+':
     print('Am sorry, you are no happy...')
 else: 
     print('You are no happy!!!')
+
+if user_data == 10 and user_bool_data:
+    print('Yeaaah boiiii')
+
+if user_data == 10 or user_bool_data:
+    print('0.5 boiiii')
