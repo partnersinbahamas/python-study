c2770ffae474ca150cea81d405121a03f9e58a62
Denys Bokov <denisbokov1703@gmail.com>
2024-09-29 19:15:44 +0200

    feat: strngs methonds

---
+++ strings.py
@@ -0,0 +1,37 @@
+# you can use most of list methods for string
+
+string = 'Python !!'
+length = len(string)
+markCount = string.count('!')
+
+print(string, length, markCount, string)
+
+print('-----')
+
+# strings only methods
+
+upperCase = string.upper()
+lowerCase = string.lower()
+isUpper = upperCase.isupper() # returns true if each char in upper case
+isLover = lowerCase.islower() # returns true if each char in lower case
+
+capitalized = lowerCase.capitalize() # make first char of string to upper case
+findPy = string.lower().find('py') # (string) returns finded index of first string char
+
+print('upper: ', upperCase, isUpper)
+print('lower: ', lowerCase, isLover)
+print('capitalize:', lowerCase, string, ': ', capitalized)
+print('find py: ', findPy)
+
+print('-----')
+
+langs = 'python, javaScript, c++'
+splitLangs = langs.split(', ')
+
+for index in range(len(splitLangs)):
+    # not works if save splitLangs[index] into variable
+    splitLangs[index] = splitLangs[index].capitalize()
+    
+langsCapitalize = ' | '.join(splitLangs)
+
+print(langs, splitLangs, langsCapitalize)
