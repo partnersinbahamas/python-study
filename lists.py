9c2dc875d7a53453254b3b3156f26773d65270fc
Denys Bokov <denisbokov1703@gmail.com>
2024-09-29 14:28:28 +0200

    feat: lists in loop

--- lists.py
+++ lists.py
@@ -1,8 +1,8 @@
 list = [1, 2, 3, 4, 5, [6, 7]]
 list[0] = 10
 lastElement = list[-1][-1]
-
 print(list, lastElement)
+
 print('-----')
 
 copy = list.copy()
@@ -21,5 +21,43 @@ nums.sort() # returns nothing, works with original array
 nums.reverse() # returns nothing, works with original array
 print(nums.count(5)) # (element): returns count of element in list
 length = len(nums) # return length of list
-
 print(nums, length)
+
+print('----- lists in loops')
+for num in nums:
+    print(num)
+
+print('----- input length')
+
+lengthOf = int(input('Enter length of list (max: 5): '))
+user_result = []
+
+
+print('for loop: ')
+
+for i in range(0, lengthOf):
+  message = 'Enter value #' + str(i + 1) + ': '
+  if lengthOf > 5:
+    print('Entered value more then 5')
+    break
+  else:
+    value = input(message)
+    user_result.append(value)
+
+print('Result: ', user_result)
+user_result.clear()
+
+print('while loop: ')
+i = 0 
+
+while(i < lengthOf):
+  message = 'Enter value #' + str(i + 1) + ': '
+  if lengthOf > 5:
+    print('Entered value more then 5')
+    break
+  else:
+    value = input(message)
+    user_result.append(value)
+    i += 1
+
+print('Result: ', user_result)
