c17d40496552f612759d3e39fad13d0a02a57962
Denys Bokov <denisbokov1703@gmail.com>
2024-09-30 13:40:09 +0200

    feat: python tuples

---
+++ tuples.py
@@ -0,0 +1,17 @@
+# Hint:
+    # has most of methods from lists(arrays) 
+    # tuples similar with lists(arrays),
+    # wokrs as a constant value
+    # has less weight as a list
+
+# if one element in tuple, its became to 5 as number
+# to make it tuple add more elements, or comma at the end => (5,)
+testTuple = (5)
+print('testTuple: ', testTuple)
+
+tuple = ('string', 1, True) # creates with () or just: 'string', 1, True
+lastOfTuple = tuple[-1]
+# tuple[0] = 'change tuple' # error => tuple is constat
+print('tuple: ', tuple, lastOfTuple)
+
+# IMPORTANT: to create tuple use: (element,) | element, | (element, element) | element, element
