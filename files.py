e6de27651a92359ec7a10f3e1fb9453589b50dc2
Denys Bokov <denisbokov1703@gmail.com>
2024-10-03 14:35:22 +0200

    feat(files): work with file with appendToFile def func

--- files.py
+++ files.py
@@ -14,3 +14,14 @@
             # 'wb' - запись в бинарном режиме.
             # 'ab' - добавление в бинарном режиме.
             # 'r+b', 'w+b', 'a+b' - чтение и запись в бинарном режиме.
+my_path = 'files/pytext.txt'
+
+def appendToFile(path):
+    file = open(path, 'a') # (path, method)
+    user_value = input(str('Enter appended text to file ' + path + ': '))
+    file.write(user_value + '\n')
+
+    # close file to avoid memory leak
+    file.close
+
+appendToFile(my_path)
