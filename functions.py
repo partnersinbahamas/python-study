2d0ed2e0564c187b88045953bdd1f67ba7ef5111
Denys Bokov <denisbokov1703@gmail.com>
2024-10-03 13:26:32 +0200

    feat: findIndex def func

--- functions.py
+++ functions.py
@@ -1,2 +1,16 @@
 def pass_func():
     pass # means nothing or void
+
+def findIndex(list, item):
+    index = 0
+
+    for el in list:
+        if (el == item):
+            return index
+        index += 1
+
+    print('Element not exist.')
+    return None
+
+finded = findIndex([1, 2, 3], 3)
+print('finded index: ', finded)
