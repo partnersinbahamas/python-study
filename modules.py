e6c8180ea0a6536395e241b9f2335b7014fb9bbb
Denys Bokov <denisbokov1703@gmail.com>
2024-10-06 13:52:23 +0200

    feat: few python modules and how work with them

---
+++ modules.py
@@ -0,0 +1,32 @@
+# Python modules
+# Detailed information about all modules here:
+    #https://docs.python.org/3/py-modindex.html
+
+import time
+import datetime as dt
+import sys, os # system, operation system: modules with info about user
+import platform # os platform
+
+# posible imports too
+from math import sqrt 
+print('Square of 25', round(sqrt(25)))
+
+# time
+print('Programm works !')
+time.sleep(1) # (time: in seconds) program on pause
+print('Programm answear after sleep 3s')
+
+# datetime
+datetime = dt.datetime.now()
+# you can also take from date datetime.now()
+dt.datetime.now().time()
+dt.datetime.now().date()
+dt.datetime.now().weekday()
+
+print('Today is: ', datetime)
+
+# sys
+print('path to project: ', sys.path)
+
+# os
+print('Device info: ', os.name, platform.system())
