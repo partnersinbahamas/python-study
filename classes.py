88d04964d6b970010af04a949272fd4cbc1c9512
Denys Bokov <denisbokov1703@gmail.com>
2024-10-06 14:58:46 +0200

    feat: classes with predefined methods

--- classes.py
+++ classes.py
@@ -5,8 +5,14 @@ class Cat:
     name = None
     age = 0
 
+    # constructor
     def __init__(self, name):
         self.name = name
+
+    def set_date(self, name=None, age=None, birth=None):
+        if name: self.name = name
+        if age: self.age = age
+        if birth: self.birth = birth
 
     def info(self):
         return 'Cat ' + str(self.name + ' ') + str(self.birth)
@@ -16,8 +22,14 @@ class Dog:
     name = None
     age = 0
 
+    # constructor
     def __init__(self, name):
         self.name = name
+
+    def set_date(self, name=None, age=None, birth=None):
+        if name: self.name = name
+        if age: self.age = age
+        if birth: self.birth = birth
 
     def info(self):
         self.bark()
@@ -29,6 +41,9 @@ class Dog:
 myDog = Dog('Harry')
 myCat = Cat('Barsik')
 
+print(myDog.birth)
+myDog.set_date(None, None, '2004')
 print(myDog.info())
+
 print(myCat.info())
     
