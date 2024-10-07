e40e4afadd7cd738c01edc250f80f11f7c22484b
Denys Bokov <denisbokov1703@gmail.com>
2024-10-07 16:21:25 +0200

    feat(strings): add updates

--- strings.py
+++ strings.py
@@ -18,6 +18,10 @@ isLover = lowerCase.islower() # returns true if each char in lower case
 capitalized = lowerCase.capitalize() # make first char of string to upper case
 findPy = string.lower().find('py') # (string) returns finded index of first string char
 
+stripped = ' Hello!  '.strip() # remove spaces
+titled = 'DENYS BOKOV'.title() # make capitalize for first letter in each word: DENYS BOKOV => Denys Bokov
+
+print('stripped', stripped)
 print('upper: ', upperCase, isUpper)
 print('lower: ', lowerCase, isLover)
 print('capitalize:', lowerCase, string, ': ', capitalized)
@@ -44,3 +48,22 @@ print(word[0::2]) # (from:to = -1:step = 1) => sche
 print(word[1::2]) # (from:to = -1:step = 1) => sche
 print(word[::-1]) # reverse string
 print(word[:4:]) # until 4th index
+
+# The placeholders for our values are {}
+# you can set index inside {} like {0}, {1}
+# or names line {name}, {age}
+# use f'' to write message with variable
+
+messagesWithIndex = 'Hi! My name is {0} and am {1} years old. {0} works as a {2}'
+messageWithWords = 'Hi! My name is {name} and am {age} years old. {name} works as a {work}'
+
+formattedIndex = messagesWithIndex.format('Denys', 20, 'Frontend developer') # (index: 0, 1, 2)
+formattedWords = messageWithWords.format(name='Denys', age=20, work='Frontend developer') # (word: name, age, work)
+
+print('message: ', messagesWithIndex, messageWithWords)
+print('messages With Index: ', formattedIndex)
+print('message With Words: ', formattedWords)
+
+city = "Berlin"
+print(f'I live in {city}')
+print(f'{100.0000:.2f}') #:.(numer)f => numbers after comma => 100.0000 => 10.00
