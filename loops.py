9c79035f336fc7f52ccbfa6d5ff8fc3152e1a990
Denys Bokov <denisbokov1703@gmail.com>
2024-10-15 19:29:53 +0200

    feat(loops): add else statemend in loops with expamle

--- loops.py
+++ loops.py
@@ -1,5 +1,5 @@
-range = range(1, 11, 2) # (from, to, iterator = 1)
-for item in range:
+my_range = range(1, 11, 2) # (from, to, iterator = 1)
+for item in my_range:
   print('range i: ', item)
 
 print('------')
@@ -13,7 +13,7 @@ for char in string:
     print('char index: ', charIndex)
   charIndex += 1
 
-for i in range:
+for i in my_range:
   if i % 2 == 0:
     continue
   
@@ -35,3 +35,15 @@ while isInfinite:
   if value == 'Stop' or value == 'stop':
     print('Stop programm')
     isInfinite = False
+
+
+# you can use else statement in for, while loops
+# only works if the loop has completed completely without calling the break operator
+in_range = 5
+    
+for n in range(2, in_range, 3):
+  if (n % in_range == 2):
+    print(f'{n} is not prime.')
+    break
+else:
+  print(f'{n} is prime')
