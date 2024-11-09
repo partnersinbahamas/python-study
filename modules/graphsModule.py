364eaeda01e881fb4543ab44e7bcee5ea150c20f
Denys Bokov <denisbokov1703@gmail.com>
2024-11-09 17:58:58 +0100

    feat: create graph module

--- modules/graphsModule.py
+++ modules/graphsModule.py
@@ -1,1 +1,10 @@
 from matplotlib import pyplot
+
+def create_graph(x, y, name, multi):
+    if multi:
+        pyplot.scatter(x, y, alpha=0.5)
+        pyplot.savefig(f'graphs/{name}.png')
+    else:
+        figure = pyplot.figure()
+        pyplot.scatter(x, y, alpha=0.5)
+        figure.savefig(f'graphs/{name}.png')
