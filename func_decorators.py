2a1af5add5abcc45457d8e49ea157256b06dc712
Denys Bokov <denisbokov1703@gmail.com>
2024-12-01 13:34:42 +0100

    feat(func_decorators): add @wraps func to change func reference

--- func_decorators.py
+++ func_decorators.py
@@ -1,7 +1,12 @@
 import webbrowser as wb
+from typing import Callable
+from functools import wraps
 # Descorators uses to modify or extend the behavior of functions or methods without changing their code.
 
-def validator(func):
+# decorator
+def validator(func: Callable) -> Callable:
+    # Using wraps, we can preserve the original function name, and importantly, any documentation included in that function.
+    @wraps(func)
     def wrapper(url):
         if ('https://' in url):
             func(url)
@@ -13,4 +18,8 @@ def validator(func):
 def open_url(url):
     wb.open(url)
 
-open_url('https://www.npmjs.com/package/2ra-ui')
+# open_url('https://www.npmjs.com/package/2ra-ui')
+    
+# with @wraps: <function open_url at 0x100cad5e0>
+# without: <function validator.<locals>.wrapper at 0x1046215e0>
+print(open_url)
